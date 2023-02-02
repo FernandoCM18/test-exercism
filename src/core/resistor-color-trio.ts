c6e2ecee85947528036d1aecd2745fbde70e6579
FernandoCM18 <fernando.c.m@live.com>
2023-02-02 13:19:30 -0800

    red: Blue and grey and brown

--- src/core/resistor-color-trio.ts
+++ src/core/resistor-color-trio.ts
@@ -1,3 +1,19 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+const bandColors = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
+
+export function decodedResistorValue([first, second, last]: Array<string>): string {
+  const value = [first, second].reduce((prev, color) => prev + bandColors.indexOf(color),'');
+  const totalZeros = bandColors.indexOf(last);
+  
+  return value + ' ' + '0'.repeat(totalZeros) + 'ohms';
 }
--- src/tests/resistor-color-trio.test.ts
+++ src/tests/resistor-color-trio.test.ts
@@ -7,23 +7,23 @@ describe('Resistor Color', () => {
     )
   });
 
-  // xit('Blue and grey and brown', () => {
-  //   expect(decodedResistorValue(['blue', 'grey', 'brown'])).toEqual('680 ohms')
-  // });
+  it('Blue and grey and brown', () => {
+    expect(decodedResistorValue(['blue', 'grey', 'brown'])).toEqual('680 ohms')
+  });
 
-  // xit('Red and black and red', () => {
-  //   expect(decodedResistorValue(['red', 'black', 'red'])).toEqual('2 kiloohms')
-  // });
+  xit('Red and black and red', () => {
+    expect(decodedResistorValue(['red', 'black', 'red'])).toEqual('2 kiloohms')
+  });
 
-  // xit('Green and brown and orange', () => {
-  //   expect(decodedResistorValue(['green', 'brown', 'orange'])).toEqual(
-  //     '51 kiloohms'
-  //   )
-  // });
+  xit('Green and brown and orange', () => {
+    expect(decodedResistorValue(['green', 'brown', 'orange'])).toEqual(
+      '51 kiloohms'
+    )
+  });
 
-  // xit('Yellow and violet and yellow', () => {
-  //   expect(decodedResistorValue(['yellow', 'violet', 'yellow'])).toEqual(
-  //     '470 kiloohms'
-  //   )
-  // });
+  xit('Yellow and violet and yellow', () => {
+    expect(decodedResistorValue(['yellow', 'violet', 'yellow'])).toEqual(
+      '470 kiloohms'
+    )
+  });
 })
