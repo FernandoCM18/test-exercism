8f20853a903f03b5c4d773a48c74dffcfc125605
FernandoCM18 <fernando.c.m@live.com>
2023-02-02 13:41:20 -0800

    green: Red and black and red

--- src/core/resistor-color-trio.ts
+++ src/core/resistor-color-trio.ts
@@ -14,6 +14,11 @@ const bandColors = [
 export function decodedResistorValue([first, second, last]: Array<string>): string {
   const value = [first, second].reduce((prev, color) => prev + bandColors.indexOf(color),'');
   const totalZeros = bandColors.indexOf(last);
+  const result = value + '0'.repeat(totalZeros);
+
+  if(Number(result) >= 1000) {
+    return (Number(result) / 1000) + ' kiloohms';
+  }
   
-  return value + '0'.repeat(totalZeros) + ' ohms';
+  return result + ' ohms';
 }
