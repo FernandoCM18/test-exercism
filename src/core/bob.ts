0ddd5936a5057e50efbc4b7e652bab6fba6a1c07
FernandoCM18 <fernando.c.m@live.com>
2023-03-18 12:42:07 -0700

    green: no letters

--- src/core/bob.ts
+++ src/core/bob.ts
@@ -1,5 +1,9 @@
 export function hey(message: string): string {  
 
+  if (message.match(/^[0-9,\s]+$/)) {
+    return 'Whatever.';
+  }
+
   if (message.endsWith('?') && message === message.toUpperCase()) {
     return "Calm down, I know what I'm doing!";
   }
