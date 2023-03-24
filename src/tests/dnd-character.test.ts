9dac9d46ab7d688816aca4835d5bf34ba9eca861
FernandoCM18 <fernando.c.m@live.com>
2023-03-24 11:38:05 -0700

    test: :white_check_mark: green: Ability modifier for score 9 is -1

--- src/tests/dnd-character.test.ts
+++ src/tests/dnd-character.test.ts
@@ -18,7 +18,7 @@ describe('Ability modifier', () => {
 	it('Ability modifier for score 8 is -1', () => {
 		expect(DnDCharacter.getModifierFor(8)).toEqual(-1);
 	});
-	xit('Ability modifier for score 9 is -1', () => {
+	it('Ability modifier for score 9 is -1', () => {
 		expect(DnDCharacter.getModifierFor(9)).toEqual(-1);
 	});
 	xit('Ability modifier for score 10 is 0', () => {
