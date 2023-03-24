9c0d942364f702456db4e2148a894e138462812a
FernandoCM18 <fernando.c.m@live.com>
2023-03-24 16:15:12 -0700

    test: :white_check_mark: green: multiple spaces not detected as a word

--- src/core/word-count.ts
+++ src/core/word-count.ts
@@ -1,16 +1,16 @@
 /* eslint-disable quotes */
 export function count(phrase: string): Map<string, number> {
-	const words = phrase.toLowerCase().split(/[\s,]+/);
+	const wordRegex = new RegExp("[0-9A-Za-z?]+(?:'[0-9A-Za-z?]+)?", 'g');
+	const words = phrase.match(wordRegex);
 	const wordCount = new Map<string, number>();
 
-	if (words) {
-		words.forEach((word) => {
-			const wordWhithoutSpecialCharacters = word.replace(/[^\w\s']/gi, '');
-			const wordWhitApostrophes = wordWhithoutSpecialCharacters.replace(/^'|'$/g, '');
-			const count = wordCount.get(wordWhitApostrophes) || 0;
-			wordCount.set(wordWhitApostrophes, count + 1);
-		});
+	if (words === null) {
+		return wordCount;
 	}
-
+	words.forEach((word) => {
+		const searchWord = word.toLowerCase();
+		const countValue = wordCount.get(searchWord);
+		wordCount.set(searchWord, (countValue ?? 0) + 1);
+	});
 	return wordCount;
 }
