589f8e02a75efd558b505b35855509d2e94bdd17
FernandoCM18 <fernando.c.m@live.com>
2023-03-24 15:33:41 -0700

    test: :white_check_mark: green: with apostrophes

--- src/core/word-count.ts
+++ src/core/word-count.ts
@@ -1,12 +1,14 @@
+/* eslint-disable quotes */
 export function count(phrase: string): Map<string, number> {
 	const words = phrase.toLowerCase().split(/[\s,]+/);
 	const wordCount = new Map<string, number>();
 
 	if (words) {
 		words.forEach((word) => {
-			const wordWhithoutSpecialCharacters = word.replace(/[^\w\s]/gi, '');
-			const count = wordCount.get(wordWhithoutSpecialCharacters) || 0;
-			wordCount.set(wordWhithoutSpecialCharacters, count + 1);
+			const wordWhithoutSpecialCharacters = word.replace(/[^\w\s']/gi, '');
+			const wordWhitApostrophes = wordWhithoutSpecialCharacters.replace(/^'|'$/g, '');
+			const count = wordCount.get(wordWhitApostrophes) || 0;
+			wordCount.set(wordWhitApostrophes, count + 1);
 		});
 	}
 
