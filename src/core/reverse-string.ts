0f3c7afc7a755b0dd1068b998aba4bd9e6c7b0b4
FernandoCM18 <fernando.c.m@live.com>
2023-03-26 23:45:37 -0700

    refactor: :recycle: refactor: simplified code

--- src/core/reverse-string.ts
+++ src/core/reverse-string.ts
@@ -1,6 +1,3 @@
 export function reverse(message: string): string {
-	const splitMessage = message.split('');
-	const arrayMessage = splitMessage.reverse();
-	const joinMesage = arrayMessage.join('');
-	return joinMesage;
+	return message.split('').reverse().join('');
 }
