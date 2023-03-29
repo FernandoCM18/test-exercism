4d2b14c458803d147b3d23485ab64f4d478ae934
FernandoCM18 <fernando.c.m@live.com>
2023-03-29 13:11:34 -0700

    test: :white_check_mark: green: all zero sides is not a triangle

--- src/core/triangle.ts
+++ src/core/triangle.ts
@@ -1,8 +1,19 @@
 export class Triangle {
 	constructor(public a: number, public b: number, public c: number) {}
 
+	public isTriangle(): boolean {
+		return (
+			this.a > 0 &&
+			this.b > 0 &&
+			this.c > 0 &&
+			this.a + this.b >= this.c &&
+			this.b + this.c >= this.a &&
+			this.a + this.c >= this.b
+		);
+	}
+
 	get isEquilateral() {
-		return this.a === this.b && this.b === this.c ? true : false;
+		return this.isTriangle() && this.a === this.b && this.b === this.c ? true : false;
 	}
 
 	get isIsosceles() {
