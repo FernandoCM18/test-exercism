05447b238091b1e9755de36f5c3d02e4bc179580
FernandoCM18 <fernando.c.m@live.com>
2023-03-29 12:15:21 -0700

    test: :white_check_mark: green: transforms more values

--- src/core/etl.ts
+++ src/core/etl.ts
@@ -1,8 +1,14 @@
-export function transform(old: { [key: string]: string[] }) {
-	const [letter] = old[1][0];
-	const [value] = Object.keys(old);
-	const result = {
-		[letter.toLowerCase()]: parseInt(value),
-	};
+type OldScore = { [key: string]: string[] };
+type NewScore = { [key: string]: number };
+
+export function transform(old: OldScore): NewScore {
+	const result: NewScore = {};
+	Object.entries(old).forEach(([scores, letters]) => {
+		const score = Number(scores);
+		letters.forEach((letter) => {
+			result[letter.toLowerCase()] = score;
+		});
+	});
+
 	return result;
 }
