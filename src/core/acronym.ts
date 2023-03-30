8c29d1030a5a5bd8412f3c4ffa9b30e85ebfa671
FernandoCM18 <fernando.c.m@live.com>
2023-03-30 16:15:41 -0700

    refactor: :recycle: refactor: add arrow functions

--- src/core/acronym.ts
+++ src/core/acronym.ts
@@ -1,12 +1,10 @@
 export function parse(phrase: string): string {
-	let result = '';
-	const newPhrase = phrase.replace(/([-a-z-])([-A-Z-])/g, '$1 $2');
-	const arrPhrase = newPhrase.split(' ');
+	const removeHyphens = (word: string) => word.replace('-', '');
+	const splitPhrase = (phrase: string) => phrase.replace(/([-a-z-])([-A-Z-])/g, '$1 $2').split(' ');
+	const getFirstLetters = (word: string[]) =>
+		word.reduce((result, word) => result.concat(removeHyphens(word)[0], ''), '');
 
-	arrPhrase.forEach((word) => {
-		const wordWhithoutHyphen = word.replace('-', '');
-		result = result.concat(wordWhithoutHyphen[0]);
-	});
+	const upperCase = (word: string) => word.toUpperCase();
 
-	return result.toUpperCase();
+	return upperCase(getFirstLetters(splitPhrase(phrase)));
 }
