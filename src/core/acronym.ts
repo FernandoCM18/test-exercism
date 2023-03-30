b2c7c8f3fa00ee1f7d919e80248829e5a8bfe5ee
FernandoCM18 <fernando.c.m@live.com>
2023-03-30 15:38:46 -0700

    test: :white_check_mark: inconsistently cased phrases

--- src/core/acronym.ts
+++ src/core/acronym.ts
@@ -1,6 +1,7 @@
 export function parse(phrase: string): string {
 	let result = '';
-	const arrPhrase = phrase.split(' ');
+	const newPhrase = phrase.replace(/([a-z])([A-Z])/g, '$1 $2');
+	const arrPhrase = newPhrase.split(' ');
 
 	arrPhrase.forEach((word) => {
 		result = result.concat(word[0]);
