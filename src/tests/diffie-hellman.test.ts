7ee77e8fc44d45472fdf99db739bfd19ce3ea1c3
FernandoCM18 <fernando.c.m@live.com>
2023-04-03 15:22:40 -0700

    test: :white_check_mark: throws an error if private key is zero

--- src/tests/diffie-hellman.test.ts
+++ src/tests/diffie-hellman.test.ts
@@ -19,7 +19,7 @@ describe('diffie-hellman', () => {
         diffieHellman.getPublicKey(-1);
       }).toThrow();
     });
-    xit('throws an error if private key is zero', () => {
+    it('throws an error if private key is zero', () => {
       expect(() => {
         diffieHellman.getPublicKey(0);
       }).toThrow();
