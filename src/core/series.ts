2d64b3ed31652741330a3f801fa792e0c027530f
FernandoCM18 <fernando.c.m@live.com>
2023-04-03 13:43:43 -0700

    test: :white_check_mark: slice length cannot be zero

--- src/core/series.ts
+++ src/core/series.ts
@@ -5,6 +5,7 @@ export class Series {
   slices(sliceLength: number): number[][] {
 
     if (sliceLength > this.series.length) throw new Error('slice length cannot be greater than series length');
+    if (sliceLength <= 0) throw new Error('slice length cannot be zero');
     
     const series: number[][] = [];
     for (let i = 0; i <= this.series.length - sliceLength; i++) {
