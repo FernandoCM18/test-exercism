10583809cfb346bb9c475c86ecdd58086e45b5df
FernandoCM18 <fernando.c.m@live.com>
2023-04-03 14:41:57 -0700

    test: :white_check_mark: can calculate public key using private key

--- src/core/diffie-hellman.ts
+++ src/core/diffie-hellman.ts
@@ -8,9 +8,28 @@ export class DiffieHellman {
     if (privateKey < 0) throw new Error();
     if (privateKey === 0 || privateKey === 1) throw new Error();
     if ( privateKey === this.p || privateKey > this.p ) throw new Error();
+    const publicKey = this.modPow(this.g, privateKey, this.p);
+
+    return publicKey;
   }
 
   public getSecret(theirPublicKey: unknown, myPrivateKey: unknown): unknown {
     throw new Error('Remove this statement and implement this function');
+  }
+
+  modPow(base: number, exponent: number, modulus: number): number {
+    if (modulus === 1) {
+      return 0;
+    }
+    let result = 1;
+    base = base % modulus;
+    while (exponent > 0) {
+      if (exponent % 2 === 1) {
+        result = (result * base) % modulus;
+      }
+      exponent = Math.floor(exponent / 2);
+      base = (base * base) % modulus;
+    }
+    return result;
   }
 }
