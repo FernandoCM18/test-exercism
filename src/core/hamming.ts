8e5eea826939e24d7dc7affd32b8b8da1a5c4197
FernandoCM18 <fernando.c.m@live.com>
2023-04-08 11:18:32 -0700

    test: :white_check_mark: green: disallow first strand longer

--- src/core/hamming.ts
+++ src/core/hamming.ts
@@ -1,5 +1,6 @@
 export function compute(left: string, right: string): number {
   let counter = 0;
+  if (left.length !== right.length) throw new Error('DNA strands must be of equal length.');
   for(let i = 0; i < left.length; i++) {
     if (left[i] !== right[i]) counter++;
   }
