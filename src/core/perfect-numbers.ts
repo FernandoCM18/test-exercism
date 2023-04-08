4d0e14e23b5e314d3b02d667d57fa66bb2dcb18c
FernandoCM18 <fernando.c.m@live.com>
2023-04-08 11:48:58 -0700

    test: :white_check_mark: green: Smallest perfect number is classified correctly

--- src/core/perfect-numbers.ts
+++ src/core/perfect-numbers.ts
@@ -1,3 +1,22 @@
-export function classify(num: number) {
-  return null;
+enum Category {
+  Perfect = 'perfect',
+  Abundant = 'abundant',
+  Deficient = 'deficient',
+}
+
+export function classify(num: number): string {
+  const suma = aliquotSum(num);
+
+  if (suma === num) return Category.Perfect;
+  return '';
+}
+
+function aliquotSum(num: number) {
+  let suma = 0;
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) {
+      suma += i;
+    }
+  }
+  return suma;
 }
